955de97610546417655cc0660dcf93e56db8a58e
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-27 00:39:42 +0300

    Added secret_word method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,9 @@
 filename = File.open("5desk.txt", "r")
 
-filename.each do |row|
-	puts row
+def secret_code filename
+	if filename.to_a.sample.length.between? 5, 12
+		secret = secret_code filename
+	end
 end
+
+puts secret_code filename
