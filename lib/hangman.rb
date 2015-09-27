f91dce9d288cf81e5a52783cc1a7a06667fd2c6e
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-27 18:05:31 +0300

    Implemented OOP

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,15 +1,25 @@
-file = File.open("5desk.txt", "r")
-temp = file.to_a.sample
+class Game
+	attr_reader :game, :file, :word
 
-def random_word temp
-	while !right_length temp
-		temp = File.open("5desk.txt", "r").to_a.sample
+	def initialize
+		@game = game
+		@file = File.open("5desk.txt", "r")
+		@word = @file.to_a.sample.chomp
 	end
-	temp
+	
+	def random_word
+		while !right_length @word
+			temp = File.open("5desk.txt", "r").to_a.sample.chom
+			@word = temp
+		end
+		@word
+	end
+
+	def right_length word
+		@word.length.between? 5, 12
+	end
 end
 
-def right_length temp
-	temp.length.between? 5, 12
-end
-
-puts random_word temp
+game = Game.new
+p game.file
+p game.random_word
