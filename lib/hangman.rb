9d134388264ace0268b63c91876d9aad1ba9e413
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-27 18:24:56 +0300

    Added Player class

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,11 +5,38 @@ class Game
 		@game = game
 		@file = File.open("5desk.txt", "r")
 		@word = @file.to_a.sample.chomp
+		@ans = 0
+	end
+
+	def start
+		puts "***H A N G M A N***"
+		puts "Loading..."
+		sleep 1
+		puts "Please choose from 1 to 3."
+		puts "1 : New game"
+		puts "2 : Load saved game"
+		puts "3 : Quit"
+		@ans = gets.to_i
+		choice
+	end
+
+	def choice
+		if @ans == 1
+			random_word
+		elsif @ans == 2
+			# some code to load game
+		elsif @ans == 3
+			puts "Sorry to see you go..."
+			exit
+		else
+			puts "1, 2 or 3 please."
+			choice
+		end
 	end
 	
 	def random_word
 		while !right_length @word
-			temp = File.open("5desk.txt", "r").to_a.sample.chom
+			temp = File.open("5desk.txt", "r").to_a.sample.chomp
 			@word = temp
 		end
 		@word
@@ -20,6 +47,19 @@ class Game
 	end
 end
 
+class Player
+	attr_reader :name
+
+	def initialize name = "Stranger"
+		@name = name
+	end
+
+	def hello
+		puts "Hello #{@name}."
+	end
+end
+
 game = Game.new
-p game.file
-p game.random_word
+player = Player.new
+player.hello
+p game.start
